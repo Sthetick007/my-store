fa807f8942bf9bf3b01e97eace4fe7b27e56c7c9
Sthetick007 <148613202+Sthetick007@users.noreply.github.com>
2025-07-08 16:22:32 +0530

    Updated Store.tsx

--- client/src/components/Store.tsx
+++ client/src/components/Store.tsx
@@ -98,7 +98,7 @@ export function Store() {
               <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-white"></div>
             ) : (
               <i className="fas fa-plus text-white text-xs"></i>
-                <i className="fas fa-tv text-gray-500 text-3xl"></i>
+            )}
           </Button>
         </div>
       </CardContent>
